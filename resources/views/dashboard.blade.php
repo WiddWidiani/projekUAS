87c344cf9c76fd6c9604365f3b72075e1fd7efed
krisnadiskominfo@gmail.com <ladylokitaa@gmail.com>
2025-05-08 19:11:11 +0800

    aboutUs and Navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
+
 <body>
+    @include('header.navbar')
+
+
+
     <h1>Selamat datang, {{ Auth::user()->name }}</h1>
 
     <h2>Daftar Artikel</h2>
@@ -26,12 +34,20 @@
     {{-- Link menu --}}
     <a class="nav-link active" href="{{ route('dashboard') }}">Dashboard</a> <br>
     <a class="nav-link active" href="{{ route('articles.store') }}">Articles</a> <br>
+    <a class="nav-link active" href="{{ route('about') }}">About Us</a> <br>
     @if (Auth::user()->role === 'admin')
-    <a class="nav-link active" href="#">Articles Comment</a> <br>
-    <a class="nav-link active" href="#">Page Comments</a> <br>
-    <a class="nav-link active" href="#">Pages</a> <br>
-    <a class="nav-link active" href="{{ route('comments.index') }}">Comments</a> <br>
-    <a href=""> logoutc</a>
+        <a class="nav-link active" href="#">Articles Comment</a> <br>
+        <a class="nav-link active" href="#">Page Comments</a> <br>
+        <a class="nav-link active" href="#">Pages</a> <br>
+        <a class="nav-link active" href="{{ route('comments.index') }}">Comments</a> <br>
+        <a href=""> logoutc</a>
     @endif
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
+    </script>
 </body>
+
+
+
+
 </html>
