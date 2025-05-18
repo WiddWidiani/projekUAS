3e217315c76a39f698013c988578cbf48f11ba6b
krisnadiskominfo@gmail.com <ladylokitaa@gmail.com>
2025-05-18 11:25:46 +0800

    UPDATE : NAVBAR BAGIAN SHOW ARTIKEL

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+@section('title', 'Dashboard')
+
+
 @section('content')
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About Us - {{ config('app.name') }}</title>
+    <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         html,
