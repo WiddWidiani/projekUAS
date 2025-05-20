b260b9fcf1b423c6994a24f83560ad58e754aa66
Widiani <2355201018@triatmamulya.ac.id>
2025-05-20 13:00:25 +0700

    Test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -56,8 +56,8 @@
                     ['foto' => 'anggota1.jpg', 'nama' => 'I Putu Ananta Adi Wijaya', 'nim' => '123456789'],
                     ['foto' => 'foto1.jpg', 'nama' => 'Timotius March Saputra', 'nim' => '987654321'],
                     ['foto' => 'foto2.jpg', 'nama' => 'Krisna Adi Kusuma', 'nim' => '456789123'],
-                    ['foto' => 'foto3.jpg', 'nama' => 'Sinta Yunika', 'nim' => '321654987'],
-                    ['foto' => 'foto4.jpg', 'nama' => 'Ni Kadek Widiani', 'nim' => '159753468'],
+                    ['foto' => 'foto3.jpg', 'nama' => 'Ni Kadek Sinta Yunika Sari', 'nim' => '321654987'],
+                    ['foto' => 'foto4.jpg', 'nama' => 'Ni Ketut Widiani', 'nim' => '159753468'],
                 ];
             @endphp
 
