5e3d3baf227b68792352424b3e4e640c4b021c23
WiddWidiani <2355201018@triatmamulya.ac.id>
2025-05-20 19:56:20 +0700

    kembali ke awal

--- resources/views/detail/show.blade.php
+++ resources/views/detail/show.blade.php
@@ -4,10 +4,9 @@
 @extends('layouts.app')
 @section('title', $title)
 @section('content')
-    <h1>Ketika Kucing dan Anjing Tinggal Satu Atap: {{ $article->title }}</h1>
+    <h1>{{ $article->title }}</h1>
     <small>Ditulis pada: {{ $article->created_at->format('d M Y') }}</small>
-    <p>Kucing dan anjing sering dianggap musuh bebuyutan. Tapi kenyataannya, ketika mereka tinggal bersama, justru banyak kejadian lucu yang terjadi. Mari kita intip kisah si Meong (kucing) dan si Doggo (anjing) yang tinggal di rumah yang sama.
-{{ $article->description }}</p>
+    <p>{{ $article->description }}</p>
     <br><br>
     <a href="{{ route('dashboard') }}">Kembali ke Dashboard</a>
 @endsection
